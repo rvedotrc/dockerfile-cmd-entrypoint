339d6677863f7baa2449611f4b2fe012a4a56696
Rachel Evans <rachel@rachelevans.org>
2025-09-06 11:45:49 +0200

    feat: even simpler

--- docker_exec_predictor.rb
+++ docker_exec_predictor.rb
@@ -32,41 +32,12 @@ class DockerExecPredictor
   end
 
   def predict
-    if non_empty_string?(entrypoint_override)
-      # `--entrypoint SOMETHING` means:
-      # - ignore ENTRYPOINT
-      # - ignore CMD
-      # - exec the given entrypoint_override, passing any cmdline_args as arguments
-      ["ok", resolve_arg0([ entrypoint_override, *cmdline_args ])]
+    cmd_and_args = resolve_exec
 
-    elsif entrypoint_override == ""
-      # `--entrypoint ""` means:
-      # - ignore ENTRYPOINT
-      # - ignore CMD
-      # - exec cmdline_args[0], passing the rest as arguments
-      #   (fail with "no command specified") if no cmdline_args were given
-      if cmdline_args.empty?
-        ["no_command_specified"]
-      else
-        ["ok", resolve_arg0(cmdline_args)]
-      end
-
-    else # entrypoint_override.nil?
-      if entrypoint_from_image.nil?
-        # No `--entrypoint` and no ENTRYPOINT means:
-        # - use either cmdline_args or (if that's empty) CMD,
-        #   exec'ing the 0'th item and passing the rest as arguments
-        #   (fail with "no command specified") if neither cmdline_args nor CMD were given
-        if effective_command.empty?
-          ["no_command_specified"]
-        else
-          ["ok", resolve_arg0(effective_command)]
-        end
-      else
-        # No `--entrypoint` but with an ENTRYPOINT means:
-        # - exec the ENTRYPOINT, passing cmdline_args or (if that's empty) CMD as arguments
-        ["ok", resolve_arg0([ *entrypoint_from_image, *effective_command ])]
-      end
+    if cmd_and_args.empty?
+      ["no_command_specified"]
+    else
+      ["ok", resolve_arg0(cmd_and_args)]
     end
   end
 
@@ -77,8 +48,26 @@ class DockerExecPredictor
   attr_accessor :entrypoint_override
   attr_accessor :cmdline_args
 
-  def non_empty_string?(s)
-    !s.nil? && !s.empty?
+  def resolve_exec
+    if !entrypoint_override.nil?
+      [
+        # Use the override entrypoint, or none. Ignore ENTRYPOINT.
+        (entrypoint_override if entrypoint_override != ""),
+        # Use any cmdline_args. Ignore CMD.
+        cmdline_args,
+      ]
+    else
+      [
+        # Use ENTRYPOINT (if any)
+        entrypoint_from_image,
+        # Use cmdline_args or (if that's empty) CMD
+        if !cmdline_args.empty?
+          cmdline_args
+        else
+          cmd_from_image
+        end
+      ]
+    end.reject(&:nil?).flatten
   end
 
   def array_or_shell(s)
@@ -91,33 +80,16 @@ class DockerExecPredictor
     end
   end
 
-  def resolve(s)
-    if s.start_with? '/'
-      s
+  def resolve_arg0(arr)
+    cmd, *args = arr
+
+    if cmd.start_with?("/")
+      [cmd, *args]
     else
       # In our test data, everything is in /usr/bin.  Normally,
       # The path used may vary depending on where things are installed.
-      "/usr/bin/#{s}"
+      ["/usr/bin/#{cmd}", *args]
     end
-  end
-
-  def resolve_arg0(arr)
-    return [] if arr.empty?
-
-    [ resolve(arr[0]), *arr[1..-1] ]
-  end
-
-  def effective_command
-    @effective_command ||=
-      begin
-        if !cmdline_args.empty?
-          cmdline_args
-        elsif cmd_from_image
-          cmd_from_image
-        else
-          []
-        end
-      end
   end
 
 end
